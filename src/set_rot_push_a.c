4259a98e990ef1904716fa9c7e2bd0b8b2e6223e
Quentin Claquebec <quclaque@k0r1p1.42mulhouse.fr>
2025-02-27 16:47:48 +0100

    assew

--- src/set_rot_push_a.c
+++ src/set_rot_push_a.c
@@ -6,13 +6,13 @@
 /*   By: quclaque <quclaque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 13:38:50 by quclaque          #+#    #+#             */
-/*   Updated: 2025/02/27 14:51:15 by quclaque         ###   ########.fr       */
+/*   Updated: 2025/02/27 16:43:08 by quclaque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	set_rarb_a(t_stack **stack_a, t_stack **stack_b, int val)
+/* int	set_rarb_a(t_stack **stack_a, t_stack **stack_b, int val)
 {
 	while ((*stack_a)->value != val && find_place_in_b(*stack_b, val) > 0)
 		rr(stack_a, stack_b);
@@ -54,4 +54,76 @@ int	set_rarrb_a(t_stack **stack_a, t_stack **stack_b, int val)
 		rrb(stack_b);
 	pb(stack_a, stack_b);
 	return (-1);
+} */
+
+static void rotate_a_forward_until_val(t_stack **stack_a, int val)
+{
+    while ((*stack_a)->value != val)
+        ra(stack_a);
+}
+
+static void rotate_a_backward_until_val(t_stack **stack_a, int val)
+{
+    while ((*stack_a)->value != val)
+        rra(stack_a);
+}
+
+static void rotate_both_forward_while(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // Assumes we want forward rotation if stack_a != val and place_in_b > 0
+    while ((*stack_a)->value != val && find_place_in_b(*stack_b, val) > 0)
+        rr(stack_a, stack_b);
+}
+
+static void rotate_both_backward_while(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // Assumes backward rotation if stack_a != val and place_in_b > 0
+    while ((*stack_a)->value != val && find_place_in_b(*stack_b, val) > 0)
+        rrr(stack_a, stack_b);
+}
+
+// ------------------------------------
+//  Functions for pushing from stack A
+// ------------------------------------
+
+int set_rarb_a(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // Rotate both forward while needed, then A alone, then B alone, then pb
+    rotate_both_forward_while(stack_a, stack_b, val);
+    rotate_a_forward_until_val(stack_a, val);
+    while (find_place_in_b(*stack_b, val) > 0)
+        rb(stack_b);
+    pb(stack_a, stack_b);
+    return (-1);
+}
+
+int set_rrarrb_a(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // Rotate both backward while needed, then A alone, then B alone, then pb
+    rotate_both_backward_while(stack_a, stack_b, val);
+    rotate_a_backward_until_val(stack_a, val);
+    while (find_place_in_b(*stack_b, val) > 0)
+        rrb(stack_b);
+    pb(stack_a, stack_b);
+    return (-1);
+}
+
+int set_rrarb_a(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // A backward, then B forward, then pb
+    rotate_a_backward_until_val(stack_a, val);
+    while (find_place_in_b(*stack_b, val) > 0)
+        rb(stack_b);
+    pb(stack_a, stack_b);
+    return (-1);
+}
+
+int set_rarrb_a(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // A forward, then B backward, then pb
+    rotate_a_forward_until_val(stack_a, val);
+    while (find_place_in_b(*stack_b, val) > 0)
+        rrb(stack_b);
+    pb(stack_a, stack_b);
+    return (-1);
 }
--- src/set_rot_push_b.c
+++ src/set_rot_push_b.c
@@ -6,13 +6,13 @@
 /*   By: quclaque <quclaque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 14:09:26 by quclaque          #+#    #+#             */
-/*   Updated: 2025/02/22 14:28:33 by quclaque         ###   ########.fr       */
+/*   Updated: 2025/02/27 16:45:46 by quclaque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	set_rarb_b(t_stack **stack_a, t_stack **stack_b, int val)
+/*int	set_rarb_b(t_stack **stack_a, t_stack **stack_b, int val)
 {
 	while ((*stack_b)->value != val && find_place_in_a(*stack_a, val) > 0)
 		rr(stack_a, stack_b);
@@ -43,7 +43,7 @@ int	set_rrarb_b(t_stack **stack_a, t_stack **stack_b, int val)
 	while ((*stack_b)->value != val)
 		rb(stack_b);
 	pa(stack_a, stack_b);
-	return (-1);
+	return (-1);  
 }
 
 int	set_rarrb_b(t_stack **stack_a, t_stack **stack_b, int val)
@@ -54,4 +54,61 @@ int	set_rarrb_b(t_stack **stack_a, t_stack **stack_b, int val)
 		rrb(stack_b);
 	pa(stack_a, stack_b);
 	return (-1);
+}*/
+
+
+static void rotate_b_forward_until_val(t_stack **stack_b, int val)
+{
+    while ((*stack_b)->value != val)
+        rb(stack_b);
+}
+
+static void rotate_b_backward_until_val(t_stack **stack_b, int val)
+{
+    while ((*stack_b)->value != val)
+        rrb(stack_b);
+}
+
+int set_rarb_b(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // Rotate both forward while needed, then B alone, then A alone, then pa
+    while ((*stack_b)->value != val && find_place_in_a(*stack_a, val) > 0)
+        rr(stack_a, stack_b);
+    rotate_b_forward_until_val(stack_b, val);
+    while (find_place_in_a(*stack_a, val) > 0)
+        ra(stack_a);
+    pa(stack_a, stack_b);
+    return (-1);
+}
+
+int set_rrarrb_b(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // Rotate both backward while needed, then B alone, then A alone, then pa
+    while ((*stack_b)->value != val && find_place_in_a(*stack_a, val) > 0)
+        rrr(stack_a, stack_b);
+    rotate_b_backward_until_val(stack_b, val);
+    while (find_place_in_a(*stack_a, val) > 0)
+        rra(stack_a);
+    pa(stack_a, stack_b);
+    return (-1);
+}
+
+int set_rrarb_b(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // A backward, B forward, then pa
+    while (find_place_in_a(*stack_a, val) > 0)
+        rra(stack_a);
+    rotate_b_forward_until_val(stack_b, val);
+    pa(stack_a, stack_b);
+    return (-1);
+}
+
+int set_rarrb_b(t_stack **stack_a, t_stack **stack_b, int val)
+{
+    // A forward, B backward, then pa
+    while (find_place_in_a(*stack_a, val) > 0)
+        ra(stack_a);
+    rotate_b_backward_until_val(stack_b, val);
+    pa(stack_a, stack_b);
+    return (-1);
 }
